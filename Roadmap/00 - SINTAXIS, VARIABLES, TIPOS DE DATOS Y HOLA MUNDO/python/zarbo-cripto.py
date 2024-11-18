059168b5d64027a90210d114e748dfb49bc97267
zarbo-crypto <63320587+zarbo-crypto@users.noreply.github.com>
2024-11-18 00:00:02 -0300

    Update zarbo-cripto.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/zarbo-cripto.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/zarbo-cripto.py
@@ -1,1 +1,44 @@
+# https://python.org
+
+# Comentario en una linea
+
+"""
+Esto también es
+un comentario
+en varias líneas
+"""
+
+
+
+'''
+esto es otro comentario
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+#El uso de mayuscula no deberia cambiar un valor definido
+MY_CONSTANT = "Mi constante" # por convención
+
+#Tipo de dato que sirve para presentar variable tipo entero
+my_int = 1
+#variable tipo decimal
+my_float = 1.5 
+#variable booleana osea que solo acepta verdadero o falso
+my_bool = False
+my_bool = True
+#variable tipo cadena de texto
+my_string = "Cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+
+
+
+#imprimir en pantalla
+print("¡Hola, python!")
+
+print("type(my_int)")
+print("type(my_float)")
+print("type(my_bool)")
+print("type(my_string)")
+print("type(my_other_string)")
 
